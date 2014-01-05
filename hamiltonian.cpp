fe4b63b7a0c15891697ea6a8963baf7635ea5362
Arun Kannawadi <arunkannawadi@cmu.edu>
2014-01-05 21:45:53 +0530

    Diagonalization routine included. Bugs fixed. Refer wiki entry on Jan 5 for more details

--- hamiltonian.cpp
+++ hamiltonian.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 #include<iomanip>
+#include<bitset>
 #include<stdlib.h>
 #include<math.h>
+#include <Eigen/Dense>
+#include<Eigen/Eigenvalues>
 
 using namespace std; 
+using namespace Eigen;
 
 #define root2 1.414213562373095
+#define Nqubits 4
 
 int main()
 {
@@ -17,18 +22,27 @@ int main()
   time(&seconds);
   srand((unsigned int)seconds);
   double **symm_gauss_rand_mat_gen(int);
-  double **hamiltonian(double**, int,int);
+  MatrixXd hamiltonian(double**, int,int);
+  MatrixXd diagonalize_symmetric(MatrixXd);
   void printmat(double**,int); 
+  void printmat_eigen(MatrixXd,int);
   
-  double **J, **H;
+  double i,j;
+  double **J;
+  MatrixXd H, V;
   
   J = symm_gauss_rand_mat_gen(4);
-  
+      
   H = hamiltonian(J, 2, 4);
   
+  cout << "Random matrix J = \n";
   printmat(J,4);
-  cout << "\n \n" ; 
-  printmat(H,6);
+
+  cout << " Hamiltonian H = \n" << H << endl;
+  
+  V = diagonalize_symmetric(H);
+  cout << "\n \n The eigenvecotrs are: \n " << V << endl;
+  
   
 }  
 
@@ -80,18 +94,17 @@ int nchoosek(int n, int k)
  return (int)nck;
 }
 
-unsigned int *definite_particle_states(const unsigned int L, int m)
+/*
+long double *definite_particle_numbers(int m)
 {
  int i,k, LCm;
- unsigned int v,w,t;
- unsigned int* states; 
  int nchoosek(int,int);
  
- LCm = nchoosek(L,m);
- states = (unsigned int*)malloc(LCm*sizeof(unsigned int));
- 
- 
- v = (1 << m) - 1;
+ LCm = nchoosek(Nqubits,m);
+ long double states[(const unsigned)LCm]; 
+ long double v,w,t;
+  
+ v = (1 << m) - 1;  // will this work?
  k = 0;
  
  while(k<LCm)
@@ -104,87 +117,119 @@ unsigned int *definite_particle_states(const unsigned int L, int m)
  return states;
  
 }
+*/
 
-double **hamiltonian(double** J, int m, int L)
+bitset<Nqubits>* definite_particle_states(int m)
 {
-  unsigned int *definite_particle_states(const unsigned int,int);
+ bitset<Nqubits> u;
+ int LCm;
+ int nchoosek(int,int);
+ LCm = nchoosek(Nqubits,m);
+ bitset<Nqubits> *states = new bitset<Nqubits>[(const unsigned int)LCm];
+ 
+ int i,j,k,l;
+ for(k=0;k<m;k++)
+    u.set(k);
+ states[0] = u;
+    
+ for(l=1;l<LCm;l++)
+ {
+   for(i=0;u[i]==0;i++);
+   for(j=i;u[j]==1;j++);
+   
+   u.set(j);
+   u.reset(j-1);
+   for(k=0;k<j-i-1;k++)
+   {
+     u.set(j-k-2,0);
+   }
+   for(k=0;k<j-i-1;k++)
+   {  
+     u.set(k,1);
+   }
+   states[l] = u; 
+ }
+ 
+ return states;
+}
+
+MatrixXd hamiltonian(double** J, int m, int L)
+{
+  bitset<Nqubits> *definite_particle_states(int);
   int nchoosek(int,int);
   
   const unsigned int constL = L;  
   int LCm; 
-  int i,j,k,n1,n2;
-  unsigned int u,v,w,x,y;
+  int i,j,k,n1,n2,n0;
+  bitset<Nqubits> u,v,w,x,y;
+  bitset<Nqubits> one;
+  one.set(0);
     
-  double tmp, sigmaJ = 0;
-  for(i=0;i<L;i++)
+  double tmp, sigmaJ;
+  sigmaJ = -1;
+  for(i=1;i<L;i++)
      for(j=0;j<i;j++)
-       sigmaJ += J[i][j];
+       sigmaJ = sigmaJ + J[i][j];
+       
+  cout << "sigmaJ = " << sigmaJ << endl;
     
   LCm = nchoosek(L,m);
   
-  unsigned int *states;
-  states =  definite_particle_states(constL,m);
+  bitset<Nqubits> *states;
+  states =  definite_particle_states(m);
   
-  double **H;
-  H = (double**)malloc(LCm*sizeof(double*));
-  for(k=0;k<LCm;k++)
-    {
-      H[k] = (double*)malloc(LCm*sizeof(double));
-      if(H[k]==NULL)
-	{
-         cout << "\n error allocating H2 " << k ;
-	 break;
-	}
-    }
-
+  MatrixXd H = MatrixXd::Zero(LCm,LCm);
+  
    for(i=0;i<LCm;i++)
      {
-      for(j=0;j<i;j++)
+      for(j=LCm-1;j>i;j--)
       {
        u = states[i];
        v = states[j];       
-       w = u^v; 
        
-       x = w&(w-1);
-       y = x&(x-1);
-       
-              
-       if(y==0)
-       {
-       
-       k=0; n1=-1;
-       while(k==0)
-       {
-        k = (w&0x01) ? 1 : 0 ;
-        w = w >> 1;
-        n1++;
-       }
-       
-       k=0; n2=n1;
-       while(k==0)
-       {
-        k = (w&0x01) ? 1 : 0 ;
-        w = w >> 1;
-        n2++;
-       }
-         H[i][j] = 2*J[n1][n2];
-         H[j][i] = 2*J[n1][n2];
-       }
-       else
-       {
-         H[i][j] = 0;
-         H[j][i] = 0;
-       }     
+       for(n1=0;u[n1]==v[n1];n1++);
+       for(n2=n1+1;u[n2]==v[n2];n2++);
+
+       for(n0=n2+1;n0<L;n0++)
+         if(!(u[n0]==v[n0]))
+          n0=L+2;
+           
+       if(n0==L)
+       {       
+         H(i,j) = 2*J[n1][n2];
+         H(j,i) = 2*J[n1][n2];
+       }  
       }
       
-      H[i][i] = 0;
+      H(i,i) = 0;
       
       for(tmp=0,k=0;k<LCm;k++)
-         tmp -= H[i][k] ;
-      H[i][i] = tmp + sigmaJ;         
+         tmp -= H(i,k) ;
+      H(i,i) = tmp + sigmaJ;         
      }
      
      return H;
+}
+
+MatrixXd diagonalize_symmetric(MatrixXd A)
+{
+ if(!(A==A.transpose()))
+ {
+  cout << "\n The matrix is not symmetric and will be symmetrized" << endl;
+  if(!(A.rows()==A.cols()))
+  {
+    cout << "\n Error! The matrix is rectangular. Returning the original matrix. " << endl;
+    return A;
+  }
+   A = 0.5*(A + A.transpose()); // ensure Matrix is symmetric 
+ }
+
+ SelfAdjointEigenSolver<MatrixXd> es(A);
+ 
+ MatrixXd All;
+ All.resize(A.rows(),A.cols()+1);
+ All << es.eigenvectors(), es.eigenvalues();
+ return All;
 }
 
 void printmat(double **mat, int dim)
@@ -198,4 +243,15 @@ void printmat(double **mat, int dim)
     }
 }
 
+
+void printmat_eigen(MatrixXd mat, int dim)
+{
+  int i,j;
+  for(i=0;i<dim;i++)
+    {
+      for(j=0;j<dim;j++)
+        cout << setprecision(6) << mat(i,j) << "\t";
+      cout << "\n" ;
+    }
+}
 
